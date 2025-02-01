a8bcca962f664cb2e6ad1a365457a751953761b9
hzx444 <hiagotrader99@gmail.com>
2025-02-01 00:28:44 -0300

    Create database.js

---
+++ database.js
@@ -0,0 +1,17 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database.sqlite");
+
+// Cria a tabela de usuários
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId TEXT NOT NULL UNIQUE,
+      plano TEXT NOT NULL,
+      dataPagamento TEXT NOT NULL,
+      validoAte TEXT NOT NULL
+    )
+  `);
+});
+
+module.exports = db;
